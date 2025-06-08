ab553b749a64ceb4616fe106ae3e38c25bdbb356
safin33221 <safin33221@gmail.com>
2025-06-08 23:14:22 +0600

    ADDED: Completed Hero Section and Implement Dark and light mood Feature

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "source.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
-
+import BlogPostHero from "./components/BlogPostHero";
+import hero from "../../public/images/heroBackground.jpg";
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
+    <div className="flex flex-col items-center justify-center min-h-screen 
+     ">
+      <BlogPostHero
+        title="Unlocking AI's Potential in Content Creation"
+        excerpt="ThinkPost uses cutting-edge AI to help you write faster, smarter, and more creatively than ever before."
+        coverUrl={hero}
+        author={{
+          name: "Safin Ahmed",
+          avatar: "https://i.pravatar.cc/100?img=3",
+        }}
+        publishedAt="2025-06-08"
+        readTime="4 min read"
+        tags={["AI", "Content", "Blogging"]}
+      />
       <h1 className="text-4xl font-bold">Welcome to ThinkPost</h1>
       <p className="mt-4 text-lg text-gray-700 dark:text-gray-600">
         ThinkPost is a platform designed to help you organize your thoughts and
