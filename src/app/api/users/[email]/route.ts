4b4f8c806904df69858d04f707b9350e2cfb9639
safin33221 <safin33221@gmail.com>
2025-06-12 00:24:02 +0600

    feat: add user profile image update functionality and enhance profile page UI

--- src/app/api/users/[email]/route.ts
+++ src/app/api/users/[email]/route.ts
@@ -1,9 +1,9 @@
 "use server"
 
 import { CollectionObjects, dbConnect } from "@/lib/dbConnect"
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
 
-export async function GET(req: Request, { params }: { params: { email: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { email: string } }) {
 
     const userCollection = await dbConnect(CollectionObjects.userCollection)
     try {
@@ -19,3 +19,39 @@ export async function GET(req: Request, { params }: { params: { email: string } 
 
 
 }
+
+export async function PUT(req: NextRequest, { params }: { params: { email: string } }) {
+
+    const userCollection = await dbConnect(CollectionObjects.userCollection)
+    const { email } = params;
+    const { url } = await req.json();
+    console.log(email);
+    console.log(url);
+
+    try {
+
+        const user = await userCollection.findOne({ email })
+        if (!user) {
+            return NextResponse.json({ message: "user not found" })
+        }
+        else {
+
+            const updateResult = await userCollection.updateOne(
+                { email },
+                { $set: { avatarUrl: url } }
+            );
+            console.log(updateResult);
+            if (updateResult.matchedCount === 0) {
+                return NextResponse.json({ message: "User not found" }, { status: 404 });
+            }
+
+            return NextResponse.json({ message: "Profile image updated successfully" }, { status: 201 });
+        }
+    } catch (error) {
+        return NextResponse.json({ message: "Server error", error }, { status: 500 });
+    }
+
+
+}
+
+
