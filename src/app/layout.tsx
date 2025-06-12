650406306ba609f6380b0c7d7ac360faa4bc4481
safin33221 <safin33221@gmail.com>
2025-06-12 20:17:16 +0600

    feat: add ReactQueryProvider for managing React Query state and integrate it into the layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import NextAuthProvider from "@/Providers/NextAuthProvider";
 import { ThemeProvider } from "./components/theme-provider";
+import ReactQueryProvider from "@/Providers/ReactQueryProvider";
 
 
 
@@ -21,33 +22,26 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Global Thought",
   description: "Global Thought is a platform designed to help you organize your thoughts and ideas effectively. Whether you are brainstorming, planning, or just jotting down notes, Global Thought provides the tools you need to stay organized and focused.",
-  icons:'../../public/images/heroBackground.jpg'
+  icons: '../../public/images/heroBackground.jpg'
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased  `}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <NextAuthProvider>
-
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-          >
-          
-
-            {children}
-
-            <Toaster />
-          </ThemeProvider>
+          <ReactQueryProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="light"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </ReactQueryProvider>
         </NextAuthProvider>
       </body>
     </html>
